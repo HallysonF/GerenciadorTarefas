5c73afbbdb1e43e2fb0fe19974a7f403e6f5a30f
HallysonF <60119117+HallysonF@users.noreply.github.com>
2021-07-26 05:09:41 -0300

    atualização pom
    atualização pom

---
+++ target/generated-sources/annotations/com/esig/gerenciatar/entity/Responsavel_.java
@@ -0,0 +1,14 @@
+package com.esig.gerenciatar.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.6.v20200131-rNA", date="2021-07-26T05:06:32")
+@StaticMetamodel(Responsavel.class)
+public class Responsavel_ { 
+
+    public static volatile SingularAttribute<Responsavel, String> nome;
+    public static volatile SingularAttribute<Responsavel, Long> id;
+
+}
