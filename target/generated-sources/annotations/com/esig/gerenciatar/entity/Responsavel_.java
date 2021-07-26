0984dcc513770bdd290eee87946c34fa1ffe2d23
HallysonF <60119117+HallysonF@users.noreply.github.com>
2021-07-25 23:35:33 -0300

    target
    target deplpy

---
+++ target/generated-sources/annotations/com/esig/gerenciatar/entity/Responsavel_.java
@@ -0,0 +1,14 @@
+package com.esig.gerenciatar.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.6.v20200131-rNA", date="2021-07-25T22:16:51")
+@StaticMetamodel(Responsavel.class)
+public class Responsavel_ { 
+
+    public static volatile SingularAttribute<Responsavel, String> nome;
+    public static volatile SingularAttribute<Responsavel, Long> id;
+
+}
---
+++ target/generated-sources/annotations/com/esig/gerenciatar/entity/Tarefas_.java
@@ -0,0 +1,18 @@
+package com.esig.gerenciatar.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.6.v20200131-rNA", date="2021-07-25T22:16:51")
+@StaticMetamodel(Tarefas.class)
+public class Tarefas_ { 
+
+    public static volatile SingularAttribute<Tarefas, String> prioridade;
+    public static volatile SingularAttribute<Tarefas, String> titulo;
+    public static volatile SingularAttribute<Tarefas, Long> id;
+    public static volatile SingularAttribute<Tarefas, String> dedline;
+    public static volatile SingularAttribute<Tarefas, String> responsavel;
+    public static volatile SingularAttribute<Tarefas, String> descricao;
+
+}
